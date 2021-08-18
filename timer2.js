7021a84ff7efe147c6b8fcb596cf027922197664
Rheaazarraga <rheaazarraga@gmail.com>
2021-08-18 22:41:49 +0000

    Refactored alarm function. Converted c style for loop to a for of loop, reversed conditions of if statement, implemented beeper function for overall code modularization

---
+++ timer2.js
@@ -0,0 +1,13 @@
+const alarms = process.argv.slice(2);
+const beeper = (delay) => {
+  setTimeout(() => {
+    //process.stdout.write('\x07');
+    console.log("beep");
+  }, (delay * 1000));
+};
+for (let element of alarms) {
+  if (element > 0 || (!isNaN(Number(element)))) {
+    beeper(element);
+  }
+
+}
