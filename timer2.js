751e63a488254798bc8178d6cd19f68d70a52fc1
Rheaazarraga <rheaazarraga@gmail.com>
2021-08-18 22:43:29 +0000

    Uncommented line 5 and commented out line 6 for proper testing purposes

--- timer2.js
+++ timer2.js
@@ -1,8 +1,8 @@
 const alarms = process.argv.slice(2);
 const beeper = (delay) => {
   setTimeout(() => {
-    //process.stdout.write('\x07');
-    console.log("beep");
+    process.stdout.write('\x07');
+    //console.log("beep");
   }, (delay * 1000));
 };
 for (let element of alarms) {
