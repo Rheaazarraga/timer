ae5571a693b4e293ad3641e82f4dbaa7102ca3ff
Rhea <rheaazarraga@gmail.com>
2022-04-12 13:14:37 -0700

    lint program

--- timer1.js
+++ timer1.js
@@ -1,22 +1,29 @@
 const alarms = process.argv.slice(2); // removes the path of node and file
-
-for (let i = 0; i < alarms.length; i ++) {
-
-    // if an argument is negative, or not a number
-    if (alarms[i] < 0 || (isNaN(Number(alarms[i]))) ) {
-      continue;
-    }
-
-  setTimeout( () => {
+//for (let i = 0; i < alarms.length; i ++) {
+const beeper = (delay) => {
+  setTimeout(() => {
     //process.stdout.write('\x07');
-     console.log("beep");
-  }, (alarms[i] * 1000));
+    console.log("beep");
+  }, (delay * 1000));
+};
+for (let element of alarms) {
+  if (element > 0 || (!isNaN(Number(element)))) {
+    // if an argument is negative, or not a number
+    //if (alarms[i] < 0 || (isNaN(Number(alarms[i]))) ) {
+    beeper(element);
+  }
+  //beeper(element);
+
+  // setTimeout( () => {
+  //   //process.stdout.write('\x07');
+  //    console.log("beep");
+  // }, (element * 1000));
 }
 
 /**EDGE CASES
- * No numbers passed: It shouldn't beep and will end immediately since no beeps are 
+ * No numbers passed: It shouldn't beep and will end immediately since no beeps are
  * scheduled/ input
- * If the input is a negative number: Ignore/skip it. We can't schedule anything in 
+ * If the input is a negative number: Ignore/skip it. We can't schedule anything in
  * the past.
  * If the input is not a number: Ignore/skip it as well
  **/
